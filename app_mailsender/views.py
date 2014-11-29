0f011f38388824ba505b4b984503eed4a6a1e6e3
Your Name <you@example.com>
2014-11-29 21:50:41 +0400

    second

--- app_mailsender/views.py
+++ app_mailsender/views.py
@@ -34,8 +34,8 @@ def feedback(request):
 				send_mail(
 					subject,
 					message,
-					'noreply@noreply.ru',
-					email,
+					email,							# sender email
+					['mailsender2015@yandex.ru'], 	# my email
 				)				
 			except:
 				print('error in try block')
