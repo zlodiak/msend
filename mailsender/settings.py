a15c949d2b84507c998b553ac6654aa6e0c43da3
Your Name <you@example.com>
2014-12-01 19:59:34 +0400

    mysql compl

--- mailsender/settings.py
+++ mailsender/settings.py
@@ -61,10 +61,26 @@ WSGI_APPLICATION = 'mailsender.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mailsender',
+        'USER': 'mailsender',
+        'PASSWORD': 'mailsender',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=INNODB',
+        },
     }
 }
 
